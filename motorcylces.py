50fa876f2f5110cc6ccdc0e1bf83181c54a7024b
Ryan Walters <RyanWaltersDev@gmail.com>
2020-11-11 22:55:52 -0500

    +finish motorcycle.py

--- motorcylces.py
+++ motorcylces.py
@@ -26,3 +26,25 @@ print(motorcycles)
 #Removing an item from the list using a .del statement
 del motorcycles[3]
 print(motorcycles)
+
+#Removing an item but keeping its value using the pop() method
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+#Using the pop() method to demonstrate chronological order in a string
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycle that I bought was a brand new {last_owned.title()}.")
+
+#Using the pop() method on a specific index
+motorcycles = ['honda', 'yamaha', 'suzuki']
+second_owned = motorcycles.pop(1)
+print(f"The second motorcycle I ever owned was a real sleek {second_owned.title()}.")
+
+#Removing a specific item using the remove() method
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"I never did own a {too_expensive.title()}, though. Those bastards are way too expensive.")
